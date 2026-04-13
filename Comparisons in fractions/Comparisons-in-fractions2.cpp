df092382297b09f07c6d022c2966cc3593ae7ce4
Petrova <olga_petrova8484@mail.ru>
2026-04-13 17:00:38 +0300

    Add commit

---
+++ Comparisons in fractions/Comparisons-in-fractions2.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <numeric>
+#include <string>
+#include <windows.h>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+	void reduce()
+	{
+		int gcd = std::gcd(numerator_, denominator_);
+		numerator_ /= gcd;
+		denominator_ /= gcd;
+	}
+
+public:
+
+	Fraction(int numerator, int denominator) : numerator_(numerator), denominator_(denominator) 
+	{
+		if (denominator == 0) {
+			throw "Знаменатель не может быть равен нулю";
+
+		}
+		reduce();  
+	}
+	void lcm(const Fraction& other, int& new_numerator_,
+	int& other_new_numerator_) const
+	{
+		if (denominator_ != other.denominator_)
+		{
+			int lcm = std::lcm(denominator_, other.denominator_); // общий знаменатель, наименьшее общее кратное
+			new_numerator_ = lcm / denominator_ * numerator_;
+			other_new_numerator_ = lcm / other.denominator_ * other.numerator_;
+		};
+	}
+	bool operator == (const Fraction& other) const
+	{
+		int new_num_;
+		int other_new_num_;
+		lcm(other, new_num_, other_new_num_);
+		return (new_num_ == other_new_num_);
+	}
+	bool operator != (const Fraction& other) const
+	{
+		return !(*this == other);
+	}
+	bool operator > (const Fraction& other) const
+	{
+		int new_num_;
+		int other_new_num_;
+		lcm(other, new_num_, other_new_num_);
+		return (new_num_ > other_new_num_);
+	}
+	bool operator < (const Fraction& other) const
+	{
+		return (!(*this > other) && !(*this == other));
+	}
+	bool operator >= (const Fraction& other) const
+	{
+		return ((*this > other) && (*this == other));
+	}
+	bool operator <= (const Fraction& other) const
+	{
+		return ((*this < other) && (*this == other));
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
--- Other operations/Other_operations2.cpp
+++ Other operations/Other_operations2.cpp
@@ -3,6 +3,7 @@
 #include <string> 
 #include <cstdlib> 
 #include <stdexcept>
+#include <numeric>
 
 
 class Fraction
@@ -11,17 +12,47 @@ private:
 	int numerator_;
 	int denominator_;
 
+	void reduction()
+	{
+		int a = abs(numerator_);
+		int b = abs(denominator_);
+		while (a != 0 && b != 0)
+		{
+			if (a > b)
+			{
+				a %= b;
+			}
+			else
+			{
+				b %= a;
+			}
+		};
+		int gcd = a + b;
+		numerator_ /= gcd;
+		denominator_ /= gcd;
+		if (denominator_ < 0) {
+			numerator_ = -numerator_;
+			denominator_ = -denominator_;
+		}
+	};
 public:
-	Fraction(int numerator, int denominator)
+	Fraction(int numerator, int denominator) : numerator_(numerator), denominator_(denominator)
 	{
 		if (denominator == 0)
 		{
 			throw std::invalid_argument("Знаменатель не может быть равен 0!");
 		}
-		numerator_ = numerator;
-		denominator_ = denominator;
-		reduction();  
+		reduction();
+	};
+
+	void lcm(const Fraction& other, int& new_numerator_,
+		int& other_new_numerator_, int& new_denominator_) const
+	{
+		new_denominator_ = std::lcm(denominator_, other.denominator_); // общий знаменатель, наименьшее общее кратное
+		new_numerator_ = new_denominator_ / denominator_ * numerator_;
+		other_new_numerator_ = new_denominator_ / other.denominator_ * other.numerator_;
 	}
+
 	int get_numer() const
 	{
 		return numerator_;
@@ -30,89 +61,63 @@ public:
 	{
 		return denominator_;
 	}
-	int find_max_divider(int a, int b) const
-	{
-		a = abs(a);
-		b = abs(b);
-		while (a != 0 && b != 0)
-		{
-			if (a > b)
-				a %= b;
-			else
-				b %= a;
-		}
-		return a + b;
-	}
-	void reduction()
-	{
-		int max_divider = find_max_divider(numerator_, denominator_);
-		numerator_ /= max_divider;
-		denominator_ /= max_divider;
-		if (denominator_ < 0)
-		{
-			denominator_ = -denominator_;
-			numerator_ = -numerator_;
-		}
-	}
 
 	Fraction operator + (const Fraction& other) const
-	{
-		int common_denominator = denominator_ * other.denominator_;
-		int new_numerator = numerator_ * other.denominator_ + other.numerator_ * denominator_;
-		Fraction result(new_numerator, common_denominator);
-		result.reduction();
-		return result;
+	{		
+		int new_num_{};
+		int other_new_num_{};
+		int new_denom_{};
+		lcm(other, new_num_, other_new_num_, new_denom_);
+		int result = new_num_ + other_new_num_;
+		return Fraction(result, new_denom_);
 	}
 	Fraction operator - (const Fraction& other) const
 	{
-		int common_denominator = denominator_ * other.denominator_;
-		int new_numerator = numerator_ * other.denominator_ - other.numerator_ * denominator_;
-		Fraction result(new_numerator, common_denominator);
-		result.reduction();
-		return result;
+		int new_num_{};
+		int other_new_num_{};
+		int new_denom_{};
+		lcm(other, new_num_, other_new_num_, new_denom_);
+		int result = new_num_ - other_new_num_;
+		return Fraction(result, new_denom_);
 	}
 	Fraction operator * (const Fraction& other) const
 	{
 		int common_denominator = denominator_ * other.denominator_;
 		int new_numerator = numerator_ * other.numerator_;
-		Fraction result(new_numerator, common_denominator);
-		result.reduction();
-		return result;
+		return Fraction(new_numerator, common_denominator);
 	}
 	Fraction operator / (const Fraction& other) const
 	{
+		if (other.numerator_ == 0) {
+			throw std::invalid_argument("Деление на ноль!");
+		}
 		int common_denominator = denominator_ * other.numerator_;
 		int new_numerator = numerator_ * other.denominator_;
-		Fraction result(new_numerator, common_denominator);
-		result.reduction();
-		return result;
+		return Fraction(new_numerator, common_denominator);
 	}
 	Fraction& operator ++ ()
 	{
 		numerator_ += denominator_;
-		reduction();
 		return *this;
 	}
 	Fraction& operator -- ()
 	{
 		numerator_ -= denominator_;
-		reduction();
 		return *this;
 	}
 	Fraction operator ++ (int)
 	{
 		Fraction copy = *this;
 		numerator_ += denominator_;
-		reduction();
 		return copy;
 	}
 	Fraction operator -- (int)
 	{
 		Fraction copy = *this;
 		numerator_ -= denominator_;
-		reduction();
 		return copy;
 	}
+
 };
 
 int main(int argc, char* argv[])
@@ -132,7 +137,7 @@ int main(int argc, char* argv[])
 	Fraction f1(numerator1, denominator1);
 	Fraction f2(numerator2, denominator2);
 
-	Fraction f3 = f1 + f2;	
+	Fraction f3 = f1 + f2;
 	std::cout << f1.get_numer() << "/" << f1.get_denom() << " + " << f2.get_numer() << "/" << f2.get_denom() << " = " << f3.get_numer() << "/" << f3.get_denom() << std::endl;
 	Fraction f4 = f1 - f2;
 	std::cout << f1.get_numer() << "/" << f1.get_denom() << " - " << f2.get_numer() << "/" << f2.get_denom() << " = " << f4.get_numer() << "/" << f4.get_denom() << std::endl;
@@ -142,12 +147,12 @@ int main(int argc, char* argv[])
 	std::cout << f1.get_numer() << "/" << f1.get_denom() << " / " << f2.get_numer() << "/" << f2.get_denom() << " = " << f6.get_numer() << "/" << f6.get_denom() << std::endl;
 	Fraction f7 = (++f1) * f2;
 	std::cout << "++" << f1.get_numer() << "/" << f1.get_denom() << " * " << f2.get_numer() << "/" << f2.get_denom() << " = " << f7.get_numer() << "/" << f7.get_denom() << std::endl;
-	
+
 	std::cout << "Значение дроби 1 = " << f1.get_numer() << "/" << f1.get_denom() << std::endl;
 	Fraction f8 = (f1--) * f2;
 
 	std::cout << f1.get_numer() << "/" << f1.get_denom() << "-- * " << f2.get_numer() << "/" << f2.get_denom() << " = " << f8.get_numer() << "/" << f8.get_denom() << std::endl;
-	
+
 	std::cout << "Значение дроби 1 = " << f1.get_numer() << "/" << f1.get_denom() << std::endl;
 	return 0;
 }
